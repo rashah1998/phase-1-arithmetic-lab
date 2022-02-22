867a9689efaba1aa19d92a5ad7b5e1a888bdd39e
Rahul Shah <rashah1998@yahoo.com>
2022-02-22 18:41:02 -0500

    commit tree fd977537b3853ea1ed27f42feb94e5198eafeac3

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+
+let num1 = 31, num2 = 2, num3 = 20, num4 = 8;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil((Math.random() * 100));
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 20, 14, 8, 3);
